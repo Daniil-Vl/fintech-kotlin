85679e1fe4504a21c478c72aa49016f858b9527e
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-09-18 19:03:04 +0300

    lesson 4: add ktor dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.10"
     kotlin("plugin.serialization") version "2.0.20"
 }
 
 group = "ru.tbank"
 version = "1.0-SNAPSHOT"
+val ktorVersion: String by project
 
 repositories {
     mavenCentral()
@@ -12,6 +13,8 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
 
     testImplementation(kotlin("test"))
 }
