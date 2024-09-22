2e151ed673443f4a7a1daff0b10730066940905a
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-09-22 19:18:14 +0300

    lesson 4: add error handling in KudaGoClient

--- src/main/kotlin/ru/tbank/client_2/KudaGoClient.kt
+++ src/main/kotlin/ru/tbank/client_2/KudaGoClient.kt
@@ -11,30 +11,35 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import ru.tbank.dto_1.News
 
-class KudaGoClient() {
+class KudaGoClient {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
     private val baseUrl: String = "https://kudago.com/public-api/v1.4/news/"
     private val httpClient: HttpClient = HttpClient()
     private val json: Json = Json { ignoreUnknownKeys = true }
 
+    private fun get(count: Int, page: Int) = runBlocking {
+        httpClient.get(baseUrl) {
+            parameter("page", page.toString())
+            parameter("page_size", count)
+            parameter("order_by", "publication_date")
+            parameter("expand", "place")
+            parameter("location", "spb")
+            parameter(
+                "fields",
+                "id,title,place,description,site_url,favorites_count,comments_count,publication_date"
+            )
+        }
+    }
+
     fun getNews(count: Int = 100, page: Int = 1): List<News> {
         if (count > 100)
             throw IllegalArgumentException("Cannot load more than 100 news")
 
-        val response: HttpResponse = runBlocking {
-            httpClient.get(baseUrl) {
-                parameter("page", page.toString())
-                parameter("page_size", count)
-                parameter("order_by", "publication_date")
-                parameter("expand", "place")
-                parameter("location", "spb")
-                parameter(
-                    "fields",
-                    "id,title,place,description,site_url,favorites_count,comments_count,publication_date"
-                )
-            }
-        }
+        val response: HttpResponse = runCatching { get(count, page) }
+            .onFailure { logger.error(it.message) }
+            .getOrNull()
+            ?: return emptyList()
 
         logger.info("Get info from KudaGo API")
         logger.info("Trying to parse json from KudaGo API...")
