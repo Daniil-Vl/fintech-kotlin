4337bbc52c26c90fd8b737323f1b4f3eaae06cbd
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-09-25 18:41:34 +0300

    lesson 5: implement data sources initialization on startup

---
+++ spring-app/src/main/kotlin/ru/tbank/springapp/service/utils/DataFetcher.kt
@@ -0,0 +1,49 @@
+package ru.tbank.springapp.service.utils
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.stereotype.Service
+import ru.tbank.springapp.aspect.Timed
+import ru.tbank.springapp.client.KudagoClient
+import ru.tbank.springapp.dao.Repository
+import ru.tbank.springapp.dto.CategoryDTO
+import ru.tbank.springapp.model.Category
+import ru.tbank.springapp.model.City
+
+@Service
+private class DataFetcher(
+    private val client: KudagoClient,
+    private val categoryRepository: Repository<Category>,
+    private val cityRepository: Repository<City>
+) : CommandLineRunner {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
+    fun fetchData() {
+        logger.info("Fetching data from Kudago API...")
+
+        val categories: List<CategoryDTO> = client.getCategories()
+        logger.info("Fetched categories: {}", categories)
+
+        val cities = client.getCities()
+        logger.info("Fetched cities: {}", cities)
+
+        logger.info("Saving fetched data...")
+
+        categories.forEach { categoryDTO ->
+            categoryRepository.save(categoryDTO.slug, categoryDTO.toCategory())
+        }
+
+        cities.forEach { cityDTO ->
+            cityRepository.save(cityDTO.slug, cityDTO.toCity())
+        }
+
+        logger.info("Fetched data successfully saved")
+    }
+
+    @Timed
+    override fun run(vararg args: String?) {
+        fetchData()
+    }
+
+}
