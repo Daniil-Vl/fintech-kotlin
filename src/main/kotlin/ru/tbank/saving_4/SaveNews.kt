d1b07d793535c5596971079d6952fe204f1e5cad
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-09-26 21:37:59 +0300

    lesson 4: add exception handling int saveNews method

--- src/main/kotlin/ru/tbank/saving_4/SaveNews.kt
+++ src/main/kotlin/ru/tbank/saving_4/SaveNews.kt
@@ -3,18 +3,27 @@ package ru.tbank.saving_4
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.builtins.ListSerializer
 import kotlinx.serialization.csv.Csv
+import org.slf4j.LoggerFactory
 import ru.tbank.dto_1.News
 import java.io.File
+import java.io.IOException
 
 class NewsCsvSaver {
 
     @OptIn(ExperimentalSerializationApi::class)
     private val csv = Csv { hasHeaderRecord = true }
 
+    private val logger = LoggerFactory.getLogger(this::class.java)
+
     @OptIn(ExperimentalSerializationApi::class)
     fun saveNews(path: String, news: List<News>) {
         val serialized = csv.encodeToString(ListSerializer(News.serializer()), news)
-        File(path).writeText(serialized)
+
+        try {
+            File(path).writeText(serialized)
+        } catch (e: IOException) {
+            logger.error("Error while saving news in file $path", e)
+        }
     }
 
 }
